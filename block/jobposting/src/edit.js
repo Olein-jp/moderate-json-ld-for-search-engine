72f374f1c3d1678dd1751d8d6447622cd1f1bbfb
Olein-jp <koji.kuno@gmail.com>
2021-11-12 14:39:17 +0900

    added blockProps

--- block/jobposting/src/edit.js
+++ block/jobposting/src/edit.js
@@ -1,20 +1,18 @@
-import { RichText } from "@wordpress/block-editor";
+import { useBlockProps } from '@wordpress/block-editor'
+import { TextControl } from '@wordpress/components';
 
 const Edit = ( props ) => {
+	const blockProps = useBlockProps();
 	const {
 		attributes: { myTest },
-		className,
 		setAttributes
 	} = props;
 	return (
-		<div className={ className } >
-			<RichText
-				className='myRichTextTest'
+		<div { ...blockProps }>
+			<TextControl
+				label="myTest"
 				value={ myTest }
 				onChange={ ( newTest ) => setAttributes({ myTest: newTest }) }
-				tagName='h3'
-				placeholder='タイトルを入力'
-				keepPlaceholderOnFocus={ true }
 			/>
 		</div>
 	);
